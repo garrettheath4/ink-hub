10ebaf9aca1f4d522135349a366756b40cda94a5
Garrett Koller <gkoller@catalist.us>
2021-02-02 12:00:54 -0500

    Patch: Cleaned up hub.py code style and syntax
    according to suggestions from PyCharm

--- hub.py
+++ hub.py
@@ -3,20 +3,21 @@
 
 import sys
 import os
-picdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'pic')
-libdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'lib')
-if os.path.exists(libdir):
-    sys.path.append(libdir)
-
 import logging
-from waveshare_epd import epd2in7b
-import time
-from PIL import Image,ImageDraw,ImageFont
-import traceback
 from signal import pause
+
+# Raspberry Pi specific modules
+from waveshare_epd import epd2in7b
+from PIL import Image,ImageDraw,ImageFont
 from gpiozero import Button
 
+pic_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'pic')
+lib_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'lib')
+if os.path.exists(lib_dir):
+    sys.path.append(lib_dir)
+
 logging.basicConfig(level=logging.DEBUG)
+
 
 def main():
     # 1. Draw the display with temp 70º
@@ -57,29 +58,29 @@ class Thermostat:
         epd.init()
         epd.Clear()
 
-        font24 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 24)
-        font18 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 18)
+        font24 = ImageFont.truetype(os.path.join(pic_dir, 'Font.ttc'), 24)
+        # font18 = ImageFont.truetype(os.path.join(pic_dir, 'Font.ttc'), 18)
 
-        HBlackimage = Image.new('1', (epd.height, epd.width), 255)  # 298*126
-        HRedimage = Image.new('1', (epd.height, epd.width), 255)  # 298*126
+        # Horizontal orientation size: 298x126
+        h_black_img = Image.new('1', (epd.height, epd.width), 255)
+        h_red_img = Image.new('1', (epd.height, epd.width), 255)
 
-        drawblack = ImageDraw.Draw(HBlackimage)
-        drawred = ImageDraw.Draw(HRedimage)
+        draw_black = ImageDraw.Draw(h_black_img)
+        draw_red = ImageDraw.Draw(h_red_img)
 
-        drawblack.text((10, 0), 'Hello World', font = font24, fill = 0)
-        drawblack.text((10, 20), 'Temp: ' + str(self.target_temp), font = font24, fill = 0)
-        drawblack.line((20, 50, 70, 100), fill = 0)
-        drawblack.line((70, 50, 20, 100), fill = 0)
-        drawblack.rectangle((20, 50, 70, 100), outline = 0)
+        draw_black.text((10, 0), 'Hello World', font=font24, fill=0)
+        draw_black.text((10, 20), 'Temp: ' + str(self.target_temp), font=font24, fill=0)
+        draw_black.line((20, 50, 70, 100), fill=0)
+        draw_black.line((70, 50, 20, 100), fill=0)
+        draw_black.rectangle((20, 50, 70, 100), outline=0)
 
-        drawred.line((165, 50, 165, 100), fill = 0)
-        drawred.line((140, 75, 190, 75), fill = 0)
-        drawred.arc((140, 50, 190, 100), 0, 360, fill = 0)
-        drawred.rectangle((80, 50, 130, 100), fill = 0)
-        drawred.chord((200, 50, 250, 100), 0, 360, fill = 0)
-        epd.display(epd.getbuffer(HBlackimage), epd.getbuffer(HRedimage))
+        draw_red.line((165, 50, 165, 100), fill=0)
+        draw_red.line((140, 75, 190, 75), fill=0)
+        draw_red.arc((140, 50, 190, 100), 0, 360, fill=0)
+        draw_red.rectangle((80, 50, 130, 100), fill=0)
+        draw_red.chord((200, 50, 250, 100), 0, 360, fill=0)
+        epd.display(epd.getbuffer(h_black_img), epd.getbuffer(h_red_img))
 
 
 if __name__ == '__main__':
     main()
-
